f05dc2be789ad802cd774723e2ca4b98f6ce70f3
David Bermeo <dabzcsc@gmail.com>
2014-10-01 10:55:30 -0500

    El formulario de agregar esta listo Changes made by David Bermeo (dabzcsc@gmail.com).

--- Friends/scripts/app/add-activity.js
+++ Friends/scripts/app/add-activity.js
@@ -11,11 +11,17 @@ app.AddActivity = (function () {
         
         var $newStatus;
         var validator;
+        var $fecha;
+        var $descripcion;
+        var $proyectos;
         
         var init = function () {
             
             validator = $('#enterStatus').kendoValidator().data('kendoValidator');
             $newStatus = $('#newStatus');
+            $fecha=$('#fechaIncidentePick');
+            $descripcion = $('#newStatus');
+            $proyectos = $('#proyectoPick');
         };
         
         var show = function () {
@@ -45,11 +51,52 @@ app.AddActivity = (function () {
             }
         };
         
+        var onSelectChange = function (sel) {
+            var selected = sel.options[sel.selectedIndex].value;
+            sel.style.color = (selected == 0) ? '#b6c5c6' : '#34495e';
+        }
+        
+        var poblarProyectos=function(){
+           
+            var pro={proyectos:[]};
+             window.localStorage.setItem("proyectos",'');
+            $.ajax({
+              type: 'POST',
+              url: "http://localhost:49524/Service1.asmx/DarProyectos",
+              success: function(data){
+                    var str=data.getElementsByTagName("string")[0].childNodes[0].nodeValue;
+                   // app.showAlert(str,"");
+                  window.localStorage.setItem("proyectos",str);
+                  
+                },
+              async:false
+           });
+           var protectosStr=window.localStorage.getItem("proyectos");
+           app.showAlert(protectosStr,'');
+           if(protectosStr!==''){
+                 pro=JSON.parse(protectosStr);
+                 var pros =pro.proyectos;
+               for(var i=0;i<pros.length;i++){
+                       $('#proyectoPick')
+                      .append($("<option></option>")
+                      .attr("value",pros[i].Id)
+                      .text(pros[i].Nombre));
+                 }
+            }
+            else{
+                app.showError("Algo paso");
+            }
+            //app.showAlert(inc.incidentes[0].Descripcion,"")
+            
+        };
+        
         return {
             init: init,
             show: show,
             me: app.Users.currentUser,
-            saveActivity: saveActivity
+            saveActivity: saveActivity,
+            onSelectChange: onSelectChange,
+            poblarProyectos: poblarProyectos
         };
         
     }());
